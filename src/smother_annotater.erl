21a7caf3b2fd033787a43769fedb993c51d45ea3
Laura M. Castro <lcastro@udc.es>
2019-02-06 11:13:59 +0100

    Fixed compilation errors.

--- src/smother_annotater.erl
+++ src/smother_annotater.erl
@@ -1,45 +1,11 @@
 -module(smother_annotater).
 
--compile([export_all]).
-
--exports([annotate/2, get_forms/1, make_tmp_file/2, get_tmp/0]).
+-export([annotate/2, get_forms/1, make_tmp_file/2, get_tmp/0]).
 
 -include_lib("wrangler/include/wrangler.hrl").
 
 annotate(_File, FDict) ->
     io:format("~p~n", [FDict]).
-
-
-f(File,FDict) ->
-    wrangler_ast_server:start_ast_server(),
-    {ok,{tree,form_list,_Attrs,Content}} = api_refac:get_ast(File),
-    Result = lists:map(fun(C) -> annotate_item(C,FDict) end, Content),
-    lists:flatten(Result).
-
-annotate_item({tre,attribute,_,_} = C, _FDict) ->
-    ?PP(C) ++ "\n";
-annotate_item(C, _FDict) ->
-    io:format("------------------~n~p~n------------------~n", [C]),
-    ?PP(C) ++ "\n".
-
-    
-open_file(File) ->
-    {ok, IF} = file:open(File,[read]),
-    Content = read_file(IF,""),
-    file:close(IF),
-    Content.
-
-read_file(IF, Content) ->
-    case file:read(IF,1) of
-	eof ->
-	    lists:flatten(lists:reverse(Content));
-	{error, Reason} ->
-	    exit(Reason);
-	{ok, Data} ->
-	    read_file(IF,[Data | Content])
-    end.
-
-%% Convert a code string into a form suitable for compiling
 
 get_forms(Code) ->
     {ok, Ts, _} = erl_scan:string(Code),
